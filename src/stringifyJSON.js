e4a87ec821884cfd5f01f5e8d03546f8f0962f4a
Justin Thareja <justin.thareja@gmail.com>
2015-04-25 19:33:47 -0600

    completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,64 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+ if (obj === null) {
+    console.log('typeof object is null');
+    return 'null';
+  }
+
+  if (typeof obj === 'function') {
+    console.log('typeof obj is function')
+    return undefined;
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    console.log('typeof obj is number or boolean')
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    console.log('typeof obj is string')
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    console.log('obj is an array');
+    var result = '['
+
+    for (var i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        result += ',';
+      }
+    }
+
+    result += ']'
+    return result;
+  }
+
+  if (typeof obj === 'object') {
+      var result = "{";
+      var keys = Object.keys(obj);
+
+    for(var i = 0; i < keys.length; i++){
+      if(typeof obj[keys[i]] === 'function' || typeof obj[keys[i]] === 'undefined') { 
+        continue;
+      }
+
+      result += stringifyJSON(keys[i]) + ":" + stringifyJSON(obj[keys[i]]);
+      if(i < keys.length - 1) {
+        result += ',';
+      } 
+    }
+
+    result += '}';
+
+    return result
+
+  }
+
+
 };
+
+
